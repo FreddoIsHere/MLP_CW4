463f06c55e054ce7ced3040223095d99dab4b64a
FreddoIsHere <fkelbel@ymail.com>
2020-01-29 16:53:45 +0000

    Simple provider/saver

--- data_providers.py
+++ data_providers.py
@@ -1,82 +1,37 @@
 import pickle
-import gzip
 import numpy as np
-import os
 from mlp import DEFAULT_SEED
 
 
 class DataProvider(object):
-    """Generic data provider."""
 
-    def __init__(self, inputs, targets, batch_size, max_num_batches=-1,
-                 shuffle_order=True, rng=None):
-        """Create a new data provider object.
-        Args:
-            inputs (ndarray): Array of data input features of shape
-                (num_data, input_dim).
-            targets (ndarray): Array of data output targets of shape
-                (num_data, output_dim) or (num_data,) if output_dim == 1.
-            batch_size (int): Number of data points to include in each batch.
-            max_num_batches (int): Maximum number of batches to iterate over
-                in an epoch. If `max_num_batches * batch_size > num_data` then
-                only as many batches as the data can be split into will be
-                used. If set to -1 all of the data will be used.
-            shuffle_order (bool): Whether to randomly permute the order of
-                the data before each epoch.
-            rng (RandomState): A seeded random number generator.
-        """
-        self.inputs = inputs
-        self.targets = targets
-        self.batch_size = batch_size
-        assert max_num_batches != 0 and not max_num_batches < -1, (
-            'max_num_batches should be -1 or > 0')
-        self.max_num_batches = max_num_batches
-        # maximum possible number of batches is equal to number of whole times
-        # batch_size divides in to the number of data points which can be
-        # found using integer division
-        possible_num_batches = self.inputs.shape[0] // batch_size
-        if self.max_num_batches == -1:
-            self.num_batches = possible_num_batches
-        else:
-            self.num_batches = min(self.max_num_batches, possible_num_batches)
-        self.shuffle_order = shuffle_order
-        if rng is None:
-            rng = np.random.RandomState(DEFAULT_SEED)
-        self.rng = rng
-        self.reset()
+    def __init__(self, file, shuffle_factor=1, seed=DEFAULT_SEED):
+        self.file = open(file, "rb")
+        self.shuffle_factor = shuffle_factor
+        np.random.seed = seed
 
-    def __iter__(self):
-        """Implements Python iterator interface.
-        This should return an object implementing a `next` method which steps
-        through a sequence returning one element at a time and raising
-        `StopIteration` when at the end of the sequence. Here the object
-        returned is the DataProvider itself.
-        """
-        return self
+    def get_map(self):
+        objs = []
+        for _ in range(self.shuffle_factor):
+            try:
+                objs.append(pickle.load(self.file))
+            except EOFError:
+                self.file.close()
+                break
+        return objs[np.random.randint(low=0, high=len(objs)-1)]
 
-    def reset(self):
-        """Resets the provider to the initial state to use in a new epoch."""
-        self._curr_batch = 0
-        if self.shuffle_order:
-            self.shuffle()
+    def __del__(self):
+        self.file.close()
 
-    def shuffle(self):
-        """Randomly shuffles order of data."""
-        new_order = self.rng.permutation(self.inputs.shape[0])
-        self.inputs = self.inputs[new_order]
-        self.targets = self.targets[new_order]
 
-    def next(self):
-        """Returns next data batch or raises `StopIteration` if at end."""
-        if self._curr_batch + 1 > self.num_batches:
-            # no more batches in current iteration through data set so reset
-            # the dataset for another pass and indicate iteration is at end
-            self.reset()
-            raise StopIteration()
-        # create an index slice corresponding to current batch number
-        batch_slice = slice(self._curr_batch * self.batch_size,
-                            (self._curr_batch + 1) * self.batch_size)
-        inputs_batch = self.inputs[batch_slice]
-        targets_batch = self.targets[batch_slice]
-        self._curr_batch += 1
-        return inputs_batch, targets_batch
+class DataSaver(object):
+
+    def __init__(self, file):
+        self.file = open(self.file, "wb")
+
+    def save_map(self, data_map):
+        pickle.dump(data_map, self.file)
+        self.file.close()
+
+    def __del__(self):
+        self.file.close()
