39aa1f3f348f5135726ed2f197690426117b6119
FreddoIsHere <fkelbel@ymail.com>
2020-02-06 23:39:45 +0000

    Environment setup

---
+++ environments.py
@@ -0,0 +1,42 @@
+import numpy as np
+import enum
+
+
+class Action(enum.Enum):
+    X = 1,
+    minus_X = 2,
+    Y = 3,
+    minus_Y = 4,
+    Z = 5,
+    minus_Z = 6,
+
+
+class Map_Environment:
+
+    def __init__(self, start_pos, target_pos):
+        self.start_pos = start_pos
+        self.state = start_pos
+        self.target = target_pos
+
+    def step(self, action):
+        self.state = self.execute_action(action)
+        reward = 0.0
+        reward -= np.sum(np.square(self.state - self.target))
+        done = self.state == self.target
+        if done:
+            reward += 10.0
+        return self.state, reward, done, {}
+
+    def execute_action(self, action):
+        return {
+            Action.X: self.state + np.array([1, 0, 0]),
+            Action.minus_X: self.state + np.array([-1, 0, 0]),
+            Action.Y: self.state + np.array([0, 1, 0]),
+            Action.minus_Y: self.state + np.array([0, -1, 0]),
+            Action.Z: self.state + np.array([0, 0, 1]),
+            Action.minus_Z: self.state + np.array([0, 0, -1])
+        }[action]
+
+    def reset(self):
+        self.state = self.start_pos
+        return self.state
